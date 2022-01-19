bca90037b7c6ee2656dacd165732c8489d65d489
Michael Lobman <michael.lobman@gmail.com>
2022-01-18 21:37:47 -0500

    commit tree a201b342394ddb1af66ac3208ca6d80418a4c72a

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,15 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+       new = Freebie.create(item_name: item_name, value: value)
+       new.dev = dev
+       new.company = self
+    end
+
+    def self.oldest_company
+        oldest = self.minimum("founding_year")
+        self.where("founding_year = ?", oldest)
+    end
 end
