4b8245322148dcb4f8eb9f17b972bae7f5c50310
Michael Lobman <michael.lobman@gmail.com>
2022-01-18 21:38:03 -0500

    Complete assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,12 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Hoodie", company_id: 4, dev_id: 1)
+Freebie.create(item_name: "Hoodie", company_id: 3, dev_id: 2)
+Freebie.create(item_name: "Hoodie", company_id: 2, dev_id: 3)
+Freebie.create(item_name: "Hoodie", company_id: 1, dev_id: 4)
+
+
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
